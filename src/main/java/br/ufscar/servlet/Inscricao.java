747649b4de487f594e386f26518c3903b466f737
gmsantos <gabriel.ms1@hotmail.com>
2017-02-22 01:38:45 -0300

    Finish :)

--- src/main/java/br/ufscar/servlet/Inscricao.java
+++ src/main/java/br/ufscar/servlet/Inscricao.java
@@ -4,18 +4,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.io.IOException;
+import org.apache.commons.io.IOUtils;
 
 public class Inscricao extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder data = new StringBuilder();
+        
+        ClassLoader classLoader = getClass().getClassLoader();
+        String html = "";
 
+        data.append("<p>O seu cadastro foi realizado com sucesso</p>");
+        data.append(String.format("<p>Nome: %s</p>", req.getParameter("nome")));
+        data.append(String.format("<p>CPF: %s</p>", req.getParameter("cpf")));
+        data.append(String.format("<p>Email: %s</p>", req.getParameter("email")));
+        data.append(String.format("<p>Telefone: %s</p>", req.getParameter("telefone")));
+        data.append(String.format("<p>Minicurso: %s</p>", req.getParameter("minicurso")));
+
+        try {
+            html = IOUtils.toString(classLoader.getResourceAsStream("templates/inscricao.html"), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        html = html.replace("{{{CONTENT}}}", data);
+
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter responseOutput = resp.getWriter();
+
+        responseOutput.write(html);        
     }
 
 }
--- src/main/java/br/ufscar/servlet/Minicurso.java
+++ src/main/java/br/ufscar/servlet/Minicurso.java
@@ -4,17 +4,37 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.io.IOException;
+import org.apache.commons.io.IOUtils;
 
 public class Minicurso extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder data = new StringBuilder();
+        
+        ClassLoader classLoader = getClass().getClassLoader();
+        String html = "";
 
+        data.append("<p>O cadastro do minicurso foi realizado com sucesso</p>");
+        data.append(String.format("<p>Nome: %s</p>", req.getParameter("nome")));
+        data.append(String.format("<p>Palestrante: %s</p>", req.getParameter("palestrante")));
+        data.append(String.format("<p>Data: %s</p>", req.getParameter("data")));
+        data.append(String.format("<p>Local: %s</p>", req.getParameter("local")));
+        data.append(String.format("<p>Mensagem: %s</p>", req.getParameter("mensagem")));
+
+        try {
+            html = IOUtils.toString(classLoader.getResourceAsStream("templates/minicurso.html"), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        html = html.replace("{{{CONTENT}}}", data);
+
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter responseOutput = resp.getWriter();
+
+        responseOutput.write(html);   
     }
 }
